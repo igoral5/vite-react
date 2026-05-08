454a652e4bbc62104e2dedc45252b350ecbf6d2f
Igor Alimov <ilalimov@mts.ru>
2026-05-08 13:49:22 +0300

    Add Image and scss loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import "./App.css";
+import Image from "./components/Image";
 import Recipe from "./components/Recipe";
+import dog from "./assets/images/dog.jpg";
 
 function App() {
   return (
-    <Recipe name="Сырные палочки" />
+    <>
+      <Recipe name="Сырные палочки" />
+      <Image src={dog} alt="Собака в шарфике"/>
+    </>
   );
 }
 
